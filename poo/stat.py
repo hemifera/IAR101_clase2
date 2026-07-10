d43ede42f3065a2d5ba48cc4dbc11ce7323d1130
hemifera <hrcmayen@outlook.com>
2026-07-09 19:08:43 -0600

    sumador de listas

--- poo/stat.py
+++ poo/stat.py
@@ -11,8 +11,15 @@ class Statistics:
     def promedio(self):
         return self.suma() / self.longitud()
     
-milista = [23, 3, 32, 32, 54, 36, 54645, 3]
-stat = Statistics(milista)
+milista1 = [23, 3, 32, 32, 54, 36, 54645, 3]
+milista2 = [2,76,83,353,5,6876,57,56,546]
+milista3 = [87657,56,76,575,543,543,8756,87,8]
+listas = []
+for lista in [milista1, milista2, milista3]:
+    listas.append(lista)
 
-print(f"Suma: {stat.suma()} y Longitud: {stat.longitud()}")
-print(f"Promedio: {stat.promedio():.2f}") # Redondeado a 2 decimales para que se vea limpio
+# listas.append(milista1, milista2, milista3)
+for lista in listas:
+    stat = Statistics(lista)
+    print(f"Suma: {stat.suma()} y Longitud: {stat.longitud()}")
+    print(f"Promedio: {stat.promedio():.2f}") # Redondeado a 2 decimales para que se vea limpio
